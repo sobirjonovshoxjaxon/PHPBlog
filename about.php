b8ee23ba5d8b666d33709718725d71e6fb66e1f2
shoxjaxon <shohjahonsobirjonov76@gmail.com>
2025-08-13 11:24:57 +0500

    Men PHPBlog saytini Layoutlarini qildim

---
+++ about.php
@@ -0,0 +1,20 @@
+<?php 
+
+    $title = 'About - KnightOne';
+    $pageTitle = 'About';
+
+    require 'includes/header.php';
+    require 'includes/pageTitle.php';
+?>
+    
+    <!-- About Section -->
+       <?php 
+            require 'includes/about.php';
+       ?>
+    <!-- /About Section -->
+
+<?php
+
+    require 'includes/footer.php';
+
+?>
---
+++ includes/pageTitle.php
@@ -0,0 +1,14 @@
+<!-- Page Title -->
+    <div class="page-title dark-background" data-aos="fade" style="background-image: url(assets/img/page-title-bg.webp);">
+      <div class="container position-relative">
+        <h1><?= $pageTitle; ?></h1>
+        <p>Esse dolorum voluptatum ullam est sint nemo et est ipsa porro placeat quibusdam quia assumenda numquam molestias.</p>
+        <nav class="breadcrumbs">
+          <ol>
+            <li><a href="index.php">Home</a></li>
+            <li class="current"><?= $pageTitle; ?></li>
+          </ol>
+        </nav>
+      </div>
+    </div>
+<!-- End Page Title -->
